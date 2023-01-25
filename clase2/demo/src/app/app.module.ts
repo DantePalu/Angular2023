9fecb389fba5412f977e4b49897356f3d24b49e6
Dante Palumbo <dantepalumbo03@gmail.com>
2023-01-25 11:41:38 -0300

    proyectoAngular

--- clase2/demo/src/app/app.module.ts
+++ clase2/demo/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentsComponent } from './components/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    LogInComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule
